a543530cf41f19a4af499b086147b96896a2e9eb
Elliot Lintz <elliot.lintz@gmail.com>
2019-09-22 03:43:13 +0200

    Animation vent et bateau

--- popup.js
+++ popup.js
@@ -4,27 +4,32 @@ console.log('Je suis popup.js');
 let boutonStart = document.getElementById('start');
 let boutonStop = document.getElementById('stop');
 let boutonCopy = document.getElementById('copy');
-let dataContainer = document.getElementById('dataContainer');
+let gitHubLink = document.getElementById('gitHubLink');
+let body = document.querySelector('body');
 
 // Le background est-il en train de tourner ?
 let state;
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
+    function (request, sender, sendResponse) { // WORKING
         if (request.msg == "running?yes") {
             state = true;
             boutonStart.classList.add('impossible');
         }
-        else if(request.msg == "running?no") {
+        else if(request.msg == "running?no") { // STOPPED
             state = false;
             boutonStop.classList.add('impossible');
+            body.classList.add('stopped');
         }
     }
 );
 chrome.runtime.sendMessage({ msg: "running?" });
 
 // Click sur les boutons
-boutonStart.addEventListener('click', () => {
+boutonStart.addEventListener('click', () => { // WORKING
     if (!boutonStart.classList.contains('impossible')) {
+        body.classList.remove('stopped');
+        body.classList.add('stoppedEnd');
+        setTimeout(() => body.classList.remove('stoppedEnd'), 4000);
         boutonStart.classList.add('impossible');
         boutonStop.classList.remove('impossible');
         chrome.runtime.sendMessage({ msg: "startBackground" });
@@ -32,11 +37,16 @@ boutonStart.addEventListener('click', () => {
     }
 });
 
-boutonStop.addEventListener('click', () => {
+boutonStop.addEventListener('click', () => { // STOPPED
     if (!boutonStop.classList.contains('impossible')) {
+        body.classList.add('stopped');
         boutonStop.classList.add('impossible');
         boutonStart.classList.remove('impossible');
         chrome.runtime.sendMessage({ msg: "stopBackground" });
         console.log('Stop Background');
     }
+});
+
+gitHubLink.addEventListener('click', () =>{
+    chrome.tabs.create({ url: "https://github.com/Elliot67/HigherOrLower" });
 });
